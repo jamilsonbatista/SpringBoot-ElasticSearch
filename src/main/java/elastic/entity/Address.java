6587a27570da1c454971cae2a2dfd9b9a049d6d4
ali.sahinogullari-at-084192572081 <VdKwDbXQLoWw0npnMhbq3+E2aR0wPkVvOhRsaUUsl7A=>
2018-01-07 00:07:33 +0300

    Address entities are added.

--- src/main/java/elastic/entity/Address.java
+++ src/main/java/elastic/entity/Address.java
@@ -1,21 +1,118 @@
 package elastic.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
 /**
  * Created by Mehmet Ali Sahinogullari on 06-Jan-18.
  */
+@Entity
+@Table(name = "ADDRESS")
 public class Address {
 
-    private  String id;
+    @Id
+    @GeneratedValue()
+    @Column(name = "ID")
+    private String id;
 
-    private String Country;
+    @Column(name = "COUNTRY_ID", nullable = false)
+    private String countryId;
 
-    private  String city;
+    @JsonIgnore
+    @JoinColumn(name = "COUNTRY_ID", referencedColumnName = "ID")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Country country;
 
-    private  String town;
+    @Column(name = "CITY_ID", nullable = false)
+    private String cityId;
+
+    @JsonIgnore
+    @JoinColumn(name = "CITY_ID", referencedColumnName = "ID", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private City city;
+
+    @Column(name = "TOWN_ID", nullable = false)
+    private String townId;
+
+    @JsonIgnore
+    @JoinColumn(name = "TOWN_ID", referencedColumnName = "ID", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Town town;
 
     private String apartment;
 
     private String apartmentNo;
 
+    public String getId() {
+        return id;
+    }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(String countryId) {
+        this.countryId = countryId;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    public String getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(String cityId) {
+        this.cityId = cityId;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    public String getTownId() {
+        return townId;
+    }
+
+    public void setTownId(String townId) {
+        this.townId = townId;
+    }
+
+    public Town getTown() {
+        return town;
+    }
+
+    public void setTown(Town town) {
+        this.town = town;
+    }
+
+    public String getApartment() {
+        return apartment;
+    }
+
+    public void setApartment(String apartment) {
+        this.apartment = apartment;
+    }
+
+    public String getApartmentNo() {
+        return apartmentNo;
+    }
+
+    public void setApartmentNo(String apartmentNo) {
+        this.apartmentNo = apartmentNo;
+    }
 }
--- src/main/java/elastic/entity/Country.java
+++ src/main/java/elastic/entity/Country.java
@@ -1,7 +1,37 @@
 package elastic.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * Created by Mehmet Ali Sahinogullari on 06-Jan-18.
  */
+@Entity
+@Table(name = "COUNTRY")
 public class Country {
+
+    @Id
+    @Column(name = "ID")
+    private  String id;
+
+    @Column(name = "NAME", nullable =  false)
+    private  String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
