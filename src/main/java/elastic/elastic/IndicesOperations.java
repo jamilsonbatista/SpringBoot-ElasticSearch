472e9c94214e888a4010e30903eb3d1eb4c9d58f
Sahinogullari <mehmetalisahinogullari@hotmail.com>
2018-01-22 22:19:24 +0300

    Indices Operations

---
+++ src/main/java/elastic/elastic/IndicesOperations.java
@@ -0,0 +1,39 @@
+package elastic.elastic;
+
+import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsResponse;
+import org.elasticsearch.client.Client;
+
+/**
+ * @author  mali.sahin on 22.01.2018.
+ */
+
+public class IndicesOperations {
+
+    private final Client client;
+
+    public IndicesOperations(Client client) {
+        this.client = client;
+    }
+
+    public  boolean checkIndexExists(String name) {
+        IndicesExistsResponse response = client.admin().indices().prepareExists(name).execute().actionGet();
+        return  response.isExists();
+    }
+
+    public  void createIndex(String name) {
+        client.admin().indices().prepareCreate(name).execute().actionGet();
+    }
+
+    public void deleteIndex(String name) {
+        client.admin().indices().prepareDelete(name).execute().actionGet();
+    }
+
+    public void closeIndex(String name) {
+        client.admin().indices().prepareClose(name).execute().actionGet();
+    }
+
+    public void  openIndex(String name){
+        client.admin().indices().prepareOpen(name).execute().actionGet();
+    }
+
+}
